42bb13608176324df5258ed27aa4142e26c136f3
Gabriel Costa <gcasilva@amazon.com>
2023-12-20 16:03:06 -0800

    Fixes to Scoutsuite automation test

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -8,6 +8,9 @@ PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 
 cd ${PROJECT_PATH}
 
+# Retrieve the AWS account ID and store it in a variable
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+
 cleanup_region() {
     echo "Cleanup running in region: $1"
     export AWS_DEFAULT_REGION=$1
@@ -25,31 +28,25 @@ cleanup_all_regions() {
 
 run_scoutsuite() {
     #Create Scoutsuite security scan custom rule
-    python3 .project_automation/functional_tests/create-scoutsuite-custom-rule.py
+    python3 .project_automation/functional_tests/scoutsuite/create-scoutsuite-custom-rule.py
     # Execute Scoutsuite security scan
-    scout aws -r us-east-1 --ruleset .project_automation/functional_tests/abi-scoutsuite-custom-ruleset.json --no-browser --max-rate 5 --max-workers 5
+    scout aws -r us-east-1 --ruleset .project_automation/functional_tests/scoutsuite/abi-scoutsuite-custom-ruleset.json --no-browser --max-rate 5 --max-workers 5 -f
     # Upload Scoutsuite security scan results to S3 bucket named scoutsuite-results-aws-AWS-ACCOUNT-ID
-    python3 .project_automation/functional_tests/process-scoutsuite-report.py
+    python3 .project_automation/functional_tests/scoutsuite/process-scoutsuite-report.py
     # Delete taskcat e2e test resources
     taskcat test clean ALL
+    process_scoutsuite_report
 }
 
 process_scoutsuite_report() {
     # Check Scoutsuite security scan result for Danger level findings (Non-0 exit code)
     scoutsuite_sysout_result=$(cat scoutsuite_sysout.txt)
-    #rm scoutsuite_sysout.txt
-    if [ "$scoutsuite_sysout_result" -ne 0 ]; then
+    scoutsuite_s3_filename=$(cat scoutsuite_s3_filename.txt)
+    rm scoutsuite_sysout.txt
+    rm scoutsuite_s3_filename.txt
+    if [ "$scoutsuite_sysout_result" -ne 0 ]; then       
         # The value is non-zero, indicating Scoutsuite report needs to be checked for security issues
-        echo "Scoutsuite report contains security issues. Details below or check the S3 bucket named scoutsuite-results-aws-AWS-ACCOUNT-ID in the test account."
-        
-        # Retrieve the AWS account ID and store it in a variable
-        AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-        
-        # Path to the scoutsuite JSON file
-        FILE="./scoutsuite-report/scoutsuite-results/scoutsuite_results_aws-$AWS_ACCOUNT_ID.js"
-
-        # Extract and filter data
-        tail -n +2 scoutsuite-report/scoutsuite-results/scoutsuite_results_aws-565009154835.js | jq '.'
+        echo "Scoutsuite report contains security issues. For details please check the log messages above or the file $scoutsuite_s3_filename in the S3 bucket named scoutsuite-results-aws-$AWS_ACCOUNT_ID in the AWS test account provided by the ABI team."
         exit 1 
     fi
 }
@@ -62,7 +59,6 @@ run_test() {
     echo $AWS_DEFAULT_REGION
     taskcat test run -n
     run_scoutsuite
-    process_scoutsuite_report
 }
 # Run taskcat e2e test
 run_test
